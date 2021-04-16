804a0ced262ec90f4587ea565ed16c9155c5d622
Kevin Smith <kevin.ar.smith@gmail.com>
2021-04-16 17:16:59 -0500

    stuck_again

--- app.py
+++ app.py
@@ -7,18 +7,18 @@ app = (Flask__name__)
 mongo= Pymongo(app, url="mongo://localhost:27017/Mars_db")
 
 @app.route("/")
-    def index():
-        mars=mongo.db.Mars.find_one()
-        return render_template("index.html"),mars=mars)
+def index():
+    mars=mongo.db.Mars.find_one()
+    return render_template("index.html"),mars=mars)
 
-    @app route("/scrape")
-    def scrape():
+@app route("/scrape")
+def scrape():
 
-        mars=mongo.db.mars
-        mars_data = scrape_mars.scrape()
-        mars.update({},mars_data,upsert=True)
+    mars=mongo.db.mars
+    mars_data = scrape_mars.scrape()
+    mars.update({},mars_data,upsert=True)
 
-        return redirect("http://localhost:5000/",code=302)
+    return redirect("http://localhost:5000/",code=302)
     
     if__name=="main":
     app.run(debug=True)
