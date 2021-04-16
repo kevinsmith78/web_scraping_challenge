a073e9000ad0f9db9d3481e676518ba0eb5e8a13
Kevin Smith <kevin.ar.smith@gmail.com>
2021-04-16 13:24:13 -0500

    app

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template,redirect,jsonify
+from flask_pymongo import PyMongo
+import scrape_mars
+
+#flask_pymongo connection
+app = (Flask__name__)
+mongo= Pymongo(app, url="mongo://localhost:27017/Mars_db")
+
+@app.route("/")
+    def index():
+        mars=mongo.db.Mars.find_one()
+        return render_template("index.html"),mars=mars)
+
+    @app route("/scrape")
+    def scrape():
+
+        mars=mongo.db.mars
+        mars_data = scrape_mars.scrape()
+        mars.update({},mars_data,upsert=True)
+
+        return redirect("http://localhost:5000/",code=302)
+    
+    if__name=="main":
+    app.run(debug=True)
+
