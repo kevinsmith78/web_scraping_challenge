5ae9c9b5d024b6ad0e9449ca7f7b2b9b1958ade5
Kevin Smith <kevin.ar.smith@gmail.com>
2021-04-17 13:28:34 -0500

    changes

--- app.py
+++ app.py
@@ -4,22 +4,23 @@ import scrape_mars
 
 #flask_pymongo connection
 app = (Flask__name__)
-mongo= Pymongo(app, url="mongo://localhost:27017/Mars_db")
+app.config["MONGO_URI"] = "mongodb://localhost:27017/Mars"
+mongo= PyMongo
 
 @app.route("/")
 def index():
     mars=mongo.db.Mars.find_one()
-    return render_template("index.html"),mars=Mars)
+    return render_template("index.html"),mission=mission)
 
 @app route("/scrape")
 def scrape():
 
     mars=mongo.db.mars
     mars_data = scrape_mars.scrape()
-    mars.update({},mars_data,upsert=True)
+    mars.update({,mars_data,upsert=True})
 
     return redirect("http://localhost:5000/",code=302)
     
-    if__name=="main":
+    if__name__=="__main__":
     app.run(debug=True)
 
