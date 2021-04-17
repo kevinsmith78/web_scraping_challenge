f44946956babc13bac1355a36c4108c41f162892
Kevin Smith <kevin.ar.smith@gmail.com>
2021-04-17 13:31:49 -0500

    fixing

--- app.py
+++ app.py
@@ -10,16 +10,15 @@ mongo= PyMongo
 @app.route("/")
 def index():
     mars=mongo.db.Mars.find_one()
-    return render_template("index.html"),mission=mission)
+    return render_template("index.html"),mars=mars)
 
 @app route("/scrape")
 def scrape():
 
     mars=mongo.db.mars
     mars_data = scrape_mars.scrape()
-    mars.update({,mars_data,upsert=True})
+    mars.update({},mars_data,upsert=True)
 
-    return redirect("http://localhost:5000/",code=302)
     
     if__name__=="__main__":
     app.run(debug=True)
